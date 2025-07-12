18daa9e95632f7906debed7ff09326624556e475
tvinnie <tvinnie77@gmail.com>
2025-07-12 16:57:05 +0300

    update portfolio details

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const projects = [
         id: 1,
         title: "Childfund Kenya ",
         description: "Spearheaded the website redesign with a strong focus on modern design principles, responsive development, and the integration of advanced analytics for improved user insights.",
-        image: "/assets/img/childfund_kenya.jpg",
+        image: "assets/img/childfund_kenya.jpg",
         technologies: ["Wordpress", "Pods","ACF", "SQL", "JS", "PHP"],
         category: "Design | Development | Analytics",
         duration: "6 months",
@@ -284,7 +284,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
         
         img.addEventListener('error', function() {
-            this.style.display = 'none';
+            // this.style.display = 'none';
         });
     });
 });
